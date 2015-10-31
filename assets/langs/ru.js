11d7949aa8766f59657d8448864a35715e1d0a0b
Alexander Gruzov <gruz0.mail@gmail.com>
2015-10-31 13:42:53 +0300

    Add Russian localization

---
+++ assets/langs/ru.js
@@ -0,0 +1,17 @@
+(function() {
+    tinyMCE.addI18n('ru', {
+        'Create grid': 'Создать таблицу',
+        'Shadow': 'Тень',
+        'Style': 'Стиль',
+        'No background': 'Без фона',
+        'Background': 'Фон',
+        'Highlight': 'Подсветка',
+        'Equalize column heights': 'Выровнять высоту столбца',
+        'Add unit': 'Добавить элемент',
+        'Unit size': 'Размер блока',
+        'Remove grid': 'Удалить сетку',
+        'Remove unit': 'Удалить элемент',
+        'Column %num%': 'Столбец %num%',
+        '%size% of %sizes%': '%size% из %sizes%'
+    });
+}());
