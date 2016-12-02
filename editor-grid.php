ba0e9233be741acedb299f937210c7b81ebe7f25
Jan Sorgalla <jan.sorgalla@dotsunited.de>
2016-12-02 12:46:37 +0100

    Prepare release v3.0.0

--- editor-grid.php
+++ editor-grid.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/dotsunited/wordpress-plugin-editor-grid
 Description: WordPress editor extension to create and manage grids.
 License: MIT
 Author: Dots United GmbH
-Version: 2.0.0
+Version: 3.0.0
 Author URI: https://dotsunited.de
 */
 
